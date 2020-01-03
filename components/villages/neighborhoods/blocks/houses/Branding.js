1a75bc7551c56e8c95e019686a7c46ce54f01bc4
Steven Hanson <steven.hanson@wintechnologies.net>
2020-01-03 16:18:14 +0000

    Add branding link route

--- components/villages/neighborhoods/blocks/houses/Branding.js
+++ components/villages/neighborhoods/blocks/houses/Branding.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 
 class Branding extends React.Component {
   render() {
     return (
       <div>
-        <img className="branding" src="./images/Branding.svg" />
+        <Link to="/">
+          <img className="branding" src="./images/Branding.svg" />
+        </Link>
       </div>
     )
   }
