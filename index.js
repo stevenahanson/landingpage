ee2f78142939924387b818930f65356e1a24ed88
Steven Hanson <steven.hanson@wintechnologies.net>
2020-01-03 16:10:37 +0000

    Add demo component router

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-grid-system';
-
 import Demo from './components/villages/Demo';
 import LandingPage from './components/villages/Landing Page.js';
 
@@ -9,8 +9,12 @@ class App extends React.Component {
   render() {
     return (
       <>
-        <LandingPage/>
-        <Demo/>
+        <Router>
+          <div>
+            <Route exact path="/" component={LandingPage}/>
+            <Route path="/demo" component={Demo}/>
+          </div>
+        </Router>
       </>
     )
   }
