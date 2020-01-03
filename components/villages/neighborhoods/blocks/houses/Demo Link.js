6b905afb759fb34bae48358d9cede31be9774f78
Steven Hanson <steven.hanson@wintechnologies.net>
2020-01-03 16:13:33 +0000

    Add demo link route

--- components/villages/neighborhoods/blocks/houses/Demo Link.js
+++ components/villages/neighborhoods/blocks/houses/Demo Link.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom'; 
 
 class DemoLink extends React.Component {
   render() {
     return (
       <div>
-        <a href="#">
+        <Link to="/demo">
           <span className="demo-link">
           Demo
           </span>
-        </a>
+        </Link>
       </div>
     )
   }
