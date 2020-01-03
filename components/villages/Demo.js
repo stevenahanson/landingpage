5820771689e071caeeebb620ea10ddc19e7e0bef
Steven Hanson <steven.hanson@wintechnologies.net>
2020-01-03 15:41:44 +0000

    Add Demo component

---
+++ components/villages/Demo.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './neighborhoods/Header';
+import Footer from './neighborhoods/Footer';
+
+class Demo extends Component {
+    render() {
+        return (
+            <div className="demo">
+                <Header/>
+                <Footer/>
+            </div>
+        );
+    }
+}
+
+module.exports = Demo;
--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Container, Row, Col } from 'react-grid-system';
+
+import Demo from './components/villages/Demo';
 import LandingPage from './components/villages/Landing Page.js';
 
 class App extends React.Component {
   render() {
     return (
-      <LandingPage/>
+      <>
+        <LandingPage/>
+        <Demo/>
+      </>
     )
   }
 }
